4ed63c5db2827e33111cf87e14322972a583d018
librte-3005 <3051800.s@gmail.com>
2021-01-09 03:30:38 +0900

    aside以外簡単な修正が終わりました

--- resources/views/post/target_index.blade.php
+++ resources/views/post/target_index.blade.php
@@ -6,21 +6,22 @@
 <div class="row justify-content-center">
     <div class="col-md-8">
         <div class="card">
-            <div class="card-header">{{ __('target') }}</div>
+            <div class="card-header posts-title-bold">TARGET</div>
                 <div class="card-body">
                 <form method="post" action="">
                     {{ csrf_field() }}
                     <h1>目標を作りましょう</h1>
-                    <p>こんなふうに書いてみて</p>
-                    <p>Big:車を買う。</p>
-                    <p>Middle:就職する。</p>
-                    <textarea name="big" placeholder=""></textarea>
+                    <ul>
+                        <li>Big：最終的な大きな目標</li>
+                        <li>Middle：最終目標の前に叶えるべき目標</li>
+</ul>
+                    <textarea name="big" placeholder="車を買う〜！"></textarea>
                      <input type="hidden" name="user_id"  value="{{Auth::user()->id}}">
-                         <p><button class="btn btn-primary" type="submit">BIGを作成</button></p>
+                         <p><button class="btn made-btn-803 input-right" type="submit">BIGを作成</button></p>
 
-                    <textarea name="middle" placeholder=""></textarea>
+                    <textarea name="middle" placeholder="就職する〜"></textarea>
                      <input type="hidden" name="user_id"  value="{{Auth::user()->id}}">
-                         <p><button class="btn btn-warning"  type="submit">MIDDLEを作成</button></p>
+                         <p><button class="btn made-btn-803 input-right"  type="submit">MIDDLEを作成</button></p>
                 </form>
                 </div>
             </div>
